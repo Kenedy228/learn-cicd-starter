2b10d8bcf080de9c6b0b3049442898e91b9c1a78
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2025-12-30 18:56:25 +1000

    add linter check in ci.yml and an unused function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -8,6 +8,8 @@ import (
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
+func unused() {}
+
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
